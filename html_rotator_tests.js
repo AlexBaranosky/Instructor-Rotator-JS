ad3d3ab53ff7d75d2cc44582419bae796ded40d5
AlexBaranosky <alexander.baranosky@gmail.com>
2011-01-24 21:04:27 -0500

    removed crud from test file

--- html_rotator_tests.js
+++ html_rotator_tests.js
@@ -3,18 +3,14 @@
 	same(INSTRUCTORS_PER_ROW, 4);
  })
  
- test('', function() {
-	same([1, 2, 3, 4].min(), 1);
- })
- 
- test('', function() {
+ test('confirming eachSlice method works as I expect', function() {
 	same([1, 2, 3, 4].eachSlice(2), [[1, 2], [3, 4]]);
  })
  
  test('can rotate an array', function() {
-	same(rotate([1, 2, 3, 4], 0), [1, 2, 3, 4] );
-	same(rotate([1, 2, 3, 4], 1), [2, 3, 4, 1] );
-	same(rotate([1, 2, 3, 4], 2), [3, 4, 1, 2] );
-	same(rotate([1, 2, 3, 4], 3), [4, 1, 2, 3] );
-	same(rotate([1, 2, 3, 4], 4), [1, 2, 3, 4] );
+	same(rotate([1, 2, 3, 4], 0), [1, 2, 3, 4]);
+	same(rotate([1, 2, 3, 4], 1), [2, 3, 4, 1]);
+	same(rotate([1, 2, 3, 4], 2), [3, 4, 1, 2]);
+	same(rotate([1, 2, 3, 4], 3), [4, 1, 2, 3]);
+	same(rotate([1, 2, 3, 4], 4), [1, 2, 3, 4]);
  })
