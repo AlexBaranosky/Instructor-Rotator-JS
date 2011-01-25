7a2deba6f2a41ce956d754d62694979b762d9a24
AlexBaranosky <alexander.baranosky@gmail.com>
2011-01-24 21:03:31 -0500

    all tests of rotate and eachSlice now passing.  Had to se same instead of equals

--- html_rotator_tests.js
+++ html_rotator_tests.js
@@ -1,17 +1,20 @@
  
  test('Four instructors per row', function() {
-	equals(INSTRUCTORS_PER_ROW, 4);
+	same(INSTRUCTORS_PER_ROW, 4);
  })
  
  test('', function() {
-	equals([1, 2, 3, 4].eachSlice(2), [[1, 2], [3, 4]]);
+	same([1, 2, 3, 4].min(), 1);
+ })
+ 
+ test('', function() {
+	same([1, 2, 3, 4].eachSlice(2), [[1, 2], [3, 4]]);
  })
  
  test('can rotate an array', function() {
-	equals(rotate([1, 2, 3], 1), [2, 3, 1] );
-	equals(rotate([1, 2, 3, 4], 0), [1, 2, 3, 4] );
-	equals(rotate([1, 2, 3, 4], 1), [2, 3, 4, 1] );
-	equals(rotate([1, 2, 3, 4], 2), [3, 4, 1, 2] );
-	equals(rotate([1, 2, 3, 4], 3), [4, 1, 2, 3] );
-	equals(rotate([1, 2, 3, 4], 4), [1, 2, 3, 4] );
+	same(rotate([1, 2, 3, 4], 0), [1, 2, 3, 4] );
+	same(rotate([1, 2, 3, 4], 1), [2, 3, 4, 1] );
+	same(rotate([1, 2, 3, 4], 2), [3, 4, 1, 2] );
+	same(rotate([1, 2, 3, 4], 3), [4, 1, 2, 3] );
+	same(rotate([1, 2, 3, 4], 4), [1, 2, 3, 4] );
  })
