b94dd433eadc793c46370188bc42a5522e233429
AlexBaranosky <alexander.baranosky@gmail.com>
2011-01-26 18:14:55 -0500

    removed unused function

--- html_rotator.js
+++ html_rotator.js
@@ -1,17 +1,5 @@
 jQuery.noConflict();
 
-Date.prototype.getWeek = function() {
-    var determinedate = new Date();
-    determinedate.setFullYear(this.getFullYear(), this.getMonth(), this.getDate());
-    var D = determinedate.getDay();
-    if(D == 0) D = 7;
-    determinedate.setDate(determinedate.getDate() + (4 - D));
-    var YN = determinedate.getFullYear();
-    var ZBDoCY = Math.floor((determinedate.getTime() - new Date(YN, 0, 1, -6)) / 86400000);
-    var WN = 1 + Math.floor(ZBDoCY / 7);
-    return WN;
-}
-
 function insertNavigation() {
     var navigation_html = "<div id=\"navigation\">\n" +
                           "    <ul>\n" +
