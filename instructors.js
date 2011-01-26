387747d9966a776e7fb0a09e21b305dc1ffe6914
AlexBaranosky <alexander.baranosky@gmail.com>
2011-01-26 18:33:27 -0500

    pulled instyructors into their own file

---
+++ instructors.js
@@ -0,0 +1,172 @@
+function instructors() {
+        return [
+            {   name: "Austria - Guthenstein",
+                link: "http://www.maitreya.at/index.php?id_seiten=5",
+                imageLink: "/images/home/countries/c-austria02c.jpg"
+            },
+            {   name: "Austria - Linz",
+                link: "http://www.shaolinqigong.at/",
+                imageLink: "/images/home/countries/c-austria01.jpg"
+            },
+            {   name: "Australia",
+                link: "http://www.wahnamaustralia.com/index.htm",
+                imageLink: "/images/home/countries/c-australia01.jpg"
+            },
+            {   name: "Costa Rica",
+                link: "http://www.shaolin-wahnam-center.org",
+                imageLink: "/images/home/countries/c-blue-mountain03.jpg"
+            },
+            {   name: "Ecuador",
+                link: "http://www.wahnam.ec/",
+                imageLink: "/images/home/countries/c-quito-02.jpg"
+            },
+            {   name: "England",
+                link: "http://www.shaolinsummercamp.co.uk/",
+                imageLink: "/images/home/countries/c-england02.jpg"
+            },
+            {   name: "England Manchester",
+                link: "http://www.innesmaran.com/swm.html",
+                imageLink: "/images/home/countries/c-manchester.jpg"
+            },
+            {   name: "Finland",
+                link: "http://www.shaolin-wahnam.fi/",
+                imageLink: "/images/home/countries/c-finland01.jpg"
+            },
+            {   name: "France",
+                link: "http://www.shaolinwahnamfrance.com/",
+                imageLink: "/images/home/countries/c-eiffel02.jpg"
+            },
+            {   name: "Germany",
+                link: "http://www.shaolin-wahnam.de/hauptrahmen.htm",
+                imageLink: "/images/home/countries/c-germany01.jpg"
+            },
+            {   name: "Germany Nord",
+                link: "http://www.shaolinwahnam-nord.de/",
+                imageLink: "/images-3/places/lubeck01.jpg"
+            },
+            {   name: "Happy Healthy Centre",
+                link: "http://www.happyhealthcentre.co.uk/index.html",
+                imageLink: "/images/home/countries/c-happy-health-centre.jpg"
+            },
+            {   name: "Holland",
+                link: "http://www.shaolinholland.com/",
+                imageLink: "/images/home/countries/c-holland01.jpg"
+            },
+            {   name: "Ireland",
+                link: "http://www.smilefromtheheart.ie/",
+                imageLink: "/images/home/countries/c-ireland01.jpg"
+            },
+            {   name: "Italy",
+                link: "http://www.puntaest.com/",
+                imageLink: "/images/home/countries/c-italy01.jpg"
+            },
+            {   name: "Kuala Lumpur",
+                link: "http://shaolinwahnammalaysia.wordpress.com/",
+                imageLink: "/images-general/places/kuala-lumpur01.jpg"
+            },
+            {   name: "Las Vegas",
+                link: "http://www.eliteqigong.com/results.html",
+                imageLink: "/images/home/countries/c-las-vegas01.jpg"
+            },
+            {   name: "London",
+                link: "http://www.wahnam.blogspot.com/",
+                imageLink: "/images/home/countries/c-london01.jpg"
+            },
+            {   name: "Malaysia",
+                link: "http://www.shaolin-wahnam.org",
+                imageLink: "/images/home/countries/c-malaysia01.jpg"
+            },
+            {   name: "Mississauga, Canada",
+                link: "http://www.shaolinwm.com/",
+                imageLink: "/images/home/countries/c-mississauga.jpg"
+            },
+            {   name: "Netherlands",
+                link: "http://www.shaolinwahnam.nl/index_main.html",
+                imageLink: "/images/home/countries/c-amsterdam01.jpg"
+            },
+            {   name: "New York City",
+                link: "http://www.grandmasterwong.com/",
+                imageLink: "/images/home/countries/c-new-york04.jpg"
+            },
+            {   name: "Portugal",
+                link: "http://www.shaolin-wahnam-portugal.com/contactos.htm",
+                imageLink: "/images/home/countries/c-portugal01.jpg"
+            },
+            {   name: "Qigong Private Clinic",
+                link: "http://www.qigongprivateclinic.com/",
+                imageLink: "/images/home/countries/qigong-clinic01.jpg"
+            },
+            {   name: "Russia",
+                link: "http://www.chiflow.ru/",
+                imageLink: "/images/home/countries/russia-160.jpg"
+            },
+            {   name: "Sabah Malaysia",
+                link: "http://shaolinwahnamsabah.com",
+                imageLink: "/images/home/countries/sabah07a.jpg"
+            },
+            {   name: "Shaolin Healing",
+                link: "http://shaolinhealing.com/",
+                imageLink: "/images/home/countries/shaolin-healing01.jpg"
+            },
+            {   name: "Singapore",
+                link: "http://www.wahnamsg.com/",
+                imageLink: "/images/home/countries/c-singapore.jpg"
+            },
+            {   name: "Southern England",
+                link: "http://www.cosmosarts.com/",
+                imageLink: "/images/home/countries/c-dorset01.jpg"
+            },
+            {   name: "Spain - Alicante",
+                link: "http://www.chikungalicante.es/",
+                imageLink: "/images/home/countries/alicante01.jpg"
+            },
+            {   name: "Spain - Barcelona 1",
+                link: "http://www.chikung-bcn.com/",
+                imageLink: "/images/home/countries/c-barcelona01.jpg"
+            },
+            {   name: "Spain - Barcelona 2",
+                link: "http://www.shaolinbcn.com/",
+                imageLink: "/images/home/countries/c-barcelona02.jpg"
+            },
+            {   name: "Spain - Madrid",
+                link: "http://www.wahnammadrid.org/",
+                imageLink: "/images/home/countries/c-madrid01.jpg"
+            },
+            {   name: "Spain - San Sebastian",
+                link: "http://www.euskalnet.net/psicosalud/andorra.htm",
+                imageLink: "/images/home/countries/c-san-sebastian01.jpg"
+            },
+            {   name: "Switzerland",
+                link: "http://www.shaolin-wahnam.ch",
+                imageLink: "/images/home/countries/c-switzerland01.jpg"
+            },
+            {   name: "Switzerland 2",
+                link: "http://www.enerqi.ch/",
+                imageLink: "/images/home/countries/c-enerqi.jpg"
+            },
+            {   name: "Switzerland 3",
+                link: "http://www.taichichuan-wahnam.ch/",
+                imageLink: "/images/home/countries/switzerland04a.jpg"
+            },
+            {   name: "USA",
+                link: "http://flowingzen.com/index.html",
+                imageLink: "/images/home/countries/c-usa01.jpg"
+            },
+            {   name: "USA - West",
+                link: "http://www.eliteqigong.com/",
+                imageLink: "/images/home/countries/c-california01.jpg"
+            },
+            {   name: "Venezuela",
+                link: "http://www.shaolinwahnam.com.ve/",
+                imageLink: "/images/home/countries/venezuela02.jpg"
+            },
+            {   name: "Wahnam Tai Chi Chuan Thailand",
+                link: "http://taichichuanthailand.com/",
+                imageLink: "/images/home/countries/thailand01.jpg"
+            },
+            {   name: "Zen Treasures",
+                link: "http://www.zentreasures.com/",
+                imageLink: "/images/home/countries/c-edinburgh03.jpg"
+            }
+        ];
+	}
